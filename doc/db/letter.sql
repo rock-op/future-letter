2aa1ca2b5db20d7a38d37da465ca12a7bff6cea9
guofengzhao <guofengzhao@passiontec.cn>
2017-02-06 13:33:36 +0800

    bugfix, 修改索引

--- doc/db/letter.sql
+++ doc/db/letter.sql
@@ -9,6 +9,6 @@ CREATE TABLE `letter` (
   `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态, 0:未发送; 1:待发送; 2:发送中; 3:发送失败; 4:发送成功',
   `update_time` bigint(20) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uni_recipient_send_time` (`recipient`,`send_time`) USING BTREE,
-  KEY `idx_send_time` (`send_time`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `uni_recipient_create_time` (`recipient`,`create_time`) USING BTREE,
+  KEY `idx_send_time_status` (`send_time`,`status`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
