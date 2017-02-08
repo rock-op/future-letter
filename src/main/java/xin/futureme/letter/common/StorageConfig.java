c1092adce91aa89cd30c6de98c0743bd277e48c9
guofengzhao <guofengzhao@passiontec.cn>
2017-02-08 18:53:53 +0800

    将oss地址由内网改成外网，方便在本地调试

--- src/main/java/xin/futureme/letter/common/StorageConfig.java
+++ src/main/java/xin/futureme/letter/common/StorageConfig.java
@@ -12,6 +12,5 @@ public class StorageConfig {
   public static final String ACCESS_ID = CFG_OSS.getString("access_id");
   public static final String ACCESS_KEY = CFG_OSS.getString("access_key");
   public static final String DEFAULT_BUCKET_NAME = CFG_OSS.getString("letter_bucket_name");
-  public static final String DEFAULT_BUCKET_DOMAIN = CFG_OSS.getString("letter_bucket_domain");
-  public static final String INTERNAL_ENDPOINT = CFG_OSS.getString("internal_endpoint");
+  public static final String INTERNAL_ENDPOINT = CFG_OSS.getString("endpoint");
 }
